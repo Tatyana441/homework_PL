c98937baec68a65187588ffb35a9dd8fc821ab53
Tatyana <ivleva.smi@yandex.ru>
2022-09-16 22:50:35 +0300

    Lesson1_task

---
+++ Program.cs
@@ -0,0 +1,106 @@
+﻿// // // // See https://aka.ms/new-console-template for more information
+// // // Console.Clear();
+// // // //Console.WriteLine("Hello, World!");
+
+// // // /*Console.WriteLine("Введите число");
+// // // string numberStr = Console.ReadLine();
+// // // int number = int.Parse(numberStr);
+// // // int result = number * number;
+
+// // // Console.WriteLine($"Число {number}, Квадрат {result}");*/
+
+
+
+// // // // Console.WriteLine("Введите квадрат числа: ");
+// // // // int square = int.Parse(Console.ReadLine());
+// // // // Console.WriteLine("Введите второе число: ");
+// // // // int number = int.Parse(Console.ReadLine());
+
+// // // // if (square / number == number)
+// // // // Console.WriteLine("Да");
+
+// // // // else
+// // // // Console.WriteLine("Нет");
+
+// // // Console.WriteLine("Введите номер дня недели: ");
+// // // string numberStr = Console.ReadLine();
+
+// // // if (numberStr == "1") 
+// // // {
+// // //     Console.WriteLine("Понедельник");  
+// // // }
+// // // else if(numberStr == "2")
+// // // {
+// // //     Console.WriteLine("Вторник");
+// // // }
+// // // else if(numberStr == "3")
+// // // {
+// // //     Console.WriteLine("Среда");
+// // // }
+// // // else if(numberStr == "4")
+// // // {
+// // //     Console.WriteLine("Четверг");
+// // // }
+// // // else if(numberStr == "5")
+// // // {
+// // //     Console.WriteLine("Пятница");
+// // // }
+// // // else if(numberStr == "6")
+// // // {
+// // //     Console.WriteLine("Суббота");
+// // // }
+// // // else if(numberStr == "7")
+// // // {
+// // //     Console.WriteLine("Воскресенье");
+// // // }
+// // // else
+// // // {
+// // //     Console.WriteLine("Дня недели для этого номера нет, введите число от 1 до 7: ");
+// // // }
+
+
+// // Console.WriteLine("Введите число: ");
+// // int number = int.Parse(Console.ReadLine());
+
+// // switch (number)
+// // {
+// //     case 1:
+// //         Console.WriteLine("ПН");
+// //         break;
+// //     case 2:
+// //         Console.WriteLine("ВТ");
+// //         break;
+// //     case 3:
+// //         Console.WriteLine("СР");
+// //         break;
+// //     case 4:
+// //         Console.WriteLine("ЧТ");
+// //         break;
+// //     case 5:
+// //         Console.WriteLine("ПТ");
+// //         break;
+// //     case 6:
+// //         Console.WriteLine("СБ");
+// //         break;
+// //     case 7:
+// //         Console.WriteLine("ВС");
+// //         break;
+// //     default:
+// //         Console.WriteLine("Введите номер от 1 до 7: ");
+// //         break;
+// // }
+
+// Console.WriteLine("Введите число: ");
+// int number = int.Parse(Console.ReadLine());
+
+// for (int i = -number; i <= number; i++)
+//     Console.Write($"{i}, ");
+
+Console.WriteLine("Введите трёхзначное число: ");
+int num = int.Parse(Console.ReadLine());
+
+if (num > 99 && num < 1000)
+Console.WriteLine(num % 10);
+
+else
+    Console.WriteLine("Это не трёхзначное число");
