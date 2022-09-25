68e362d1e5881d75a9551dbfca5ab5ef6a057a59
Tatyana <ivleva.smi@yandex.ru>
2022-09-25 16:11:45 +0300

    Added tasks on seminar

---
+++ lection_max_number_from_2_number/max_number_from_2_number.cs
@@ -0,0 +1,34 @@
+int GetRandomNumber(int leftRange, int rightRange)
+{
+    Random rnd = new Random();
+    int result = rnd.Next(leftRange, rightRange); // [leftRange, rightRange]
+    return result;
+}
+
+int GetMaxDigitOfNumber(int number)
+{
+    int firstDigit = number / 10;
+    int secondDigit = number % 10;
+
+    if(firstDigit > secondDigit)
+        return firstDigit;
+    else
+        return secondDigit;    
+}
+
+int number = GetRandomNumber(10, 100);
+Console.WriteLine($"Рандомное число: {number}");
+
+int maxDigit = GetMaxDigitOfNumber(number);
+Console.WriteLine($"Максимальная цифра: {maxDigit}");
+
+
+
+
+
+
+
+
+
+
+
