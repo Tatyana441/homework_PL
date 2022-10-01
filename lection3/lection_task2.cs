e963ee736fbf80879a02db64fb0b0e5dc6afdb0f
Tatyana <ivleva.smi@yandex.ru>
2022-10-01 20:01:24 +0300

    Added lection task

--- lection3/lection_task2.cs
+++ lection3/lection_task2.cs
@@ -1,12 +1,21 @@
-String Method4(int count, string text)
-{
-    string result = String.Empty;
-    for (int i = 0; i < count; i++)
-    {
-        result = result + text;
-    }
-    return result;
-}
+// String Method4(int count, string text)
+// {
+//     string result = String.Empty;
+//     for (int i = 0; i < count; i++)
+//     {
+//         result = result + text;
+//     }
+//     return result;
+// }
 
-string res = Method4(10, "ab");
-Console.WriteLine(res);
+// string res = Method4(10, "ab");
+// Console.WriteLine(res);
+
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} * {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
