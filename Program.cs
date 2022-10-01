506df14425842850f5de9487036bbe79e657886e
Tatyana <ivleva.smi@yandex.ru>
2022-10-01 19:38:21 +0300

    Added lection

--- Program.cs
+++ Program.cs
@@ -106,3 +106,16 @@
 
 // else
 //     Console.WriteLine("Это не трёхзначное число");
+
+String Method4(int count, string text)
+{
+    string result = String.Empty;
+    for (int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+
+string res = Method4(10, ab);
+Console.WriteLine(res);
