b825e2dc8e79176bbbcdf0cca750b124ca49d496
Tatyana <ivleva.smi@yandex.ru>
2022-10-03 21:34:06 +0300

    Update 5,6,7 tasks

--- task_05/task_05.cs
+++ task_05/task_05.cs
@@ -1,17 +1,11 @@
-// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
-
-// 5 -> 2, 4
-// 8 -> 2, 4, 6, 8
-
+// Задача 10: Напишите программу, которая принимает 
+// на вход трёхзначное число и на выходе показывает 
+// вторую цифру этого числа.
 Console.Clear();
-
-Console.WriteLine("Введите число: ");
+Console.WriteLine("Введите трёхзначное число: ");
 int number = int.Parse(Console.ReadLine());
-int f_num = 1;
 
-while(f_num <= number)
-{
-if(f_num % 2 ==0)
-    Console.Write(f_num + " ");
-    f_num ++;
-}
+if (number >= 100 && number <=999)
+    Console.WriteLine(number % 100 / 10);
+else
+    Console.WriteLine("Это не трёхзначное число");
